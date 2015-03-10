549313c002bd68131c41ed586bb183b2ff156ba4
Slava Nossar <slava@presentcompany.co>
2015-03-10 22:32:33 +1100

    added nav, removed landing anim,

--- js/min/main-min.js
+++ js/min/main-min.js
@@ -1,1 +1,13 @@
-$(document).ready(function(){$(".splash-container").addClass("animate"),$(".splash-copy-container").addClass("animate"),$(".splash-copy").addClass("animate"),$(".splash-logo").addClass("animate"),$(".one").addClass("animate"),$(".two").addClass("animate"),$(".three").addClass("animate"),$(".four").addClass("animate"),$(".splash-img").addClass("animate"),$(".splash-img-shade").addClass("animate")});
+// $(document).ready(function() {
+// 	$('.splash-container').addClass('animate');
+// 	$('.splash-copy-container').addClass('animate');
+// 	$('.splash-copy').addClass('animate');
+// 	$('.splash-logo').addClass('animate');
+// 	$('.one').addClass('animate');
+// 	$('.two').addClass('animate');
+// 	$('.three').addClass('animate');
+// 	$('.four').addClass('animate');
+// 	$('.splash-img').addClass('animate');
+// 	$('.splash-img-shade').addClass('animate');
+// });
+
